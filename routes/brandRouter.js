c87eee3ae0e98427b4ecf9913589fd39b7e7bca4
Андрей Куранов <andreumironov0@gmail.com>
2022-11-27 23:17:01 +0300

    first commit

---
+++ routes/brandRouter.js
@@ -0,0 +1,11 @@
+const Router = require('express')
+const router = new Router()
+const brandController = require('../controllers/brandController')
+const checkRole = require('../middleware/checkRoleMiddleware')
+
+router.post('/', checkRole('ADMIN'), brandController.create)
+router.get('/', brandController.getAll)
+router.put('/:id', checkRole('ADMIN'), brandController.put)
+router.delete('/:id', checkRole('ADMIN'), brandController.delete)
+
+module.exports = router
